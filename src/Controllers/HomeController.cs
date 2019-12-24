45b1529d83ece0c5bd53b826c7b90fb2648fa3b3
Jeff Proefrock <jeffproe@gmail.com>
2019-12-23 19:13:38 -0500

    Added responsive sidebar to Home and Latest Posts partial (#2)

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -1,14 +1,33 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Miniblog.Core.Models;
+using Miniblog.Core.Services;
 
 namespace src.Controllers
 {
 	public class HomeController : Controller
 	{
+		private readonly IBlogService _blog;
+		public HomeController(IBlogService blog)
+		{
+			_blog = blog;
+		}
+
 		[Route("/")]
 		[OutputCache(Profile = "default")]
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
 		{
-			return View();
+			var model = new HomeViewModel();
+			var posts = await _blog.GetPosts(5);
+			foreach (var post in posts)
+			{
+				model.LatestPosts.Add(new LatestPost()
+				{
+					Title = post.Title,
+					Link = post.GetLink()
+				});
+			}
+			return View(model);
 		}
 	}
 }
---
+++ src/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Miniblog.Core.Models
+{
+	public class HomeViewModel
+	{
+		public HomeViewModel()
+		{
+			LatestPosts = new List<LatestPost>();
+		}
+
+		public List<LatestPost> LatestPosts { get; set; }
+	}
+}
